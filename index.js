6f1f30b1112dc6dbfc0afa013cf464ff20691f79
Denis Boyara <dboyarshin@yandex.ru>
2020-05-03 19:51:33 +0500

    Рендеринг данных

--- index.js
+++ index.js
@@ -16,14 +16,27 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.status(200)
-    res.render('index')
+    res.render('index', {
+        title: 'Main page',
+        isHome: true,
+    })
 })
 
-app.get('/about', (req, res) => {
+app.get('/add', (req, res) => {
     res.status(200)
-    res.render('about')
+    res.render('add', {
+        title: 'Add Course',
+        isAdd: true,
+    })
 })
 
+app.get('/courses', (req, res) => {
+    res.status(200)
+    res.render('courses', {
+        title: 'Courses',
+        isCourses: true,
+    })
+})
 
 const PORT = process.env.PORT || 3000
 
