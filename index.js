2ee24ceaafbd4985b2552e598a3fb058219f36e3
Denis Boyara <dboyarshin@yandex.ru>
2020-05-05 19:19:20 +0500

    Регистрация роутов

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
 const app = express()
 
@@ -12,31 +15,11 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
 app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.status(200)
-    res.render('index', {
-        title: 'Main page',
-        isHome: true,
-    })
-})
-
-app.get('/add', (req, res) => {
-    res.status(200)
-    res.render('add', {
-        title: 'Add Course',
-        isAdd: true,
-    })
-})
-
-app.get('/courses', (req, res) => {
-    res.status(200)
-    res.render('courses', {
-        title: 'Courses',
-        isCourses: true,
-    })
-})
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
 const PORT = process.env.PORT || 3000
 
