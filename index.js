13b6ab2daec59dc63959b3c69c7751cbd71079d5
Denis Boyara <dboyarshin@yandex.ru>
2020-05-17 19:11:06 +0500

    Добавление пользователя

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const addRoutes = require('./routes/add')
 const cardRoutes = require('./routes/card')
 const coursesRoutes = require('./routes/courses')
 
+const User = require('./models/user')
+
 const app = express()
 
 const hbs = exphbs.create({
@@ -23,6 +25,16 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('5ec1429bed74489e7ae3099b')
+        req.user = user
+        next()
+    } catch (e) {
+        console.log(e)
+    }
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
@@ -41,6 +53,16 @@ async function start (){
             useUnifiedTopology: true,
             useFindAndModify: false
         })
+
+        const candidate = await User.findOne()
+        if (!candidate) {
+            const user = new User({
+                email: 'test@test.ru',
+                name: 'Test',
+                cart: {items: []}
+            })
+            await user.save()
+        }
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
