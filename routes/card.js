79de017f04547dd7176afe2aa6a8969b619a1fc9
Denis Boyara <dboyarshin@yandex.ru>
2020-05-18 20:51:15 +0500

    Добавление товара в корзину

--- routes/card.js
+++ routes/card.js
@@ -1,11 +1,10 @@
 const {Router} = require('express')
-const Card = require('../models/card')
 const Course = require('../models/course')
 const router = Router()
 
 router.post('/add', async (req, res) => {
-  const course = await Course.getById(req.body.id)
-  await Card.add(course)
+  const course = await Course.findById(req.body.id)
+  await req.user.addToCart(course)
   res.redirect('/card')
 })
 
@@ -15,12 +14,12 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-  const card = await Card.fetch()
+  // const card = await Card.fetch()
   res.render('card', {
     title: 'Корзина',
     isCard: true,
-    courses: card.courses,
-    price: card.price
+    // courses: card.courses,
+    // price: card.price
   })
 })
 
