ef4755e682fe3a379da5151f76d99796fa531235
Denis Boyara <dboyarshin@yandex.ru>
2020-05-19 20:21:27 +0500

    Подготовка страницы заказов

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const cartRoutes = require('./routes/cart')
 const coursesRoutes = require('./routes/courses')
+const ordersRoutes = require('./routes/orders')
 
 const User = require('./models/user')
 
@@ -43,6 +44,7 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes) 
 app.use('/cart', cartRoutes)
+app.use('/orders', ordersRoutes)
 
 const PORT = process.env.PORT || 3000
 
