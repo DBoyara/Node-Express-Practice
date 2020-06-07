7d8bbce009b7352bd7e8cb775843b4ef192fddf7
Denis Boyara <dboyarshin@yandex.ru>
2020-06-07 14:46:00 +0500

    Логин пользователя

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,48 @@
 const {Router} = require('express')
-const router = Router()
 const User = require('../models/user')
-
+const router = Router()
 
 router.get('/login', async (req, res) => {
-    res.render('auth/login', {
-        title: 'Login',
-        isLogin: true
-    })
+  res.render('auth/login', {
+    title: 'Авторизация',
+    isLogin: true
+  })
 })
 
 router.get('/logout', async (req, res) => {
-    req.session.destroy(() => {
-      res.redirect('/auth/login#login')
-    })
+  req.session.destroy(() => {
+    res.redirect('/auth/login#login')
   })
-  
-  router.post('/login', async (req, res) => {
-    const user = await User.findById('5ec1429bed74489e7ae3099b')
-    req.session.user = user
-    req.session.isAuthenticated = true
-    req.session.save(err => {
-      if (err) {
-        throw err
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const {email, password} = req.body
+    const candidate = await User.findOne({ email })
+
+    if (candidate) {
+      const isSame = password === candidate.password
+
+      if (isSame) {
+        req.session.user = candidate
+        req.session.isAuthenticated = true
+        req.session.save(err => {
+          if (err) {
+            throw err
+          }
+          res.redirect('/')
+        })
+      } else {
+        res.redirect('/auth/login#login')
       }
-      res.redirect('/')
-    })
-  })
-  
+    } else {
+      res.redirect('/auth/login#login')
+    }
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 router.post('/register', async (req, res) => {
   try {
     const {email, password, repeat, name} = req.body
